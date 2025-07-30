077461f9f1848ba2c517bb9db271d7680f53ba23
abrhot <abrihott@gmail.com>
2025-07-30 23:00:31 +0300

    changes

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,11 +1,8 @@
-// src/app/(main)/layout.tsx
-
-import React from 'react';
-
-export default function MainLayout({ children }: { children: React.ReactNode }) {
+// src/app/layout.tsx
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-screen bg-white text-[#141810]">
-      {children}
-    </div>
+    <html lang="en">
+      <body>{children}</body>
+    </html>
   );
 }
