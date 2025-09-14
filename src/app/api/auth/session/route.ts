ecaf0dcb08996fbc4ae878cba93ae2383ebfb83c
abrhot <abrihott@gmail.com>
2025-09-14 23:04:36 +0300

    update pages

--- src/app/api/auth/session/route.ts
+++ src/app/api/auth/session/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/auth";
 
 export async function GET() {
   // Get the session using the NextAuth helper
