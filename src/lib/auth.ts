4f1b703075d4190e7e7e61122f7b6da46ac9e58f
abrhot <abrihott@gmail.com>
2025-09-17 23:24:56 +0300

    fix pages

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,19 +1,4 @@
 
-// src/lib/auth.ts
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { NextAuthOptions } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import prisma from "@/lib/prisma";
-import bcrypt from "bcrypt";
-export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        email: { label: "Email", type: "text" },
-        password: { label: "Password", type: "password" },
-      },
       async authorize(credentials) {
         if (!credentials?.email || !credentials.password) return null;
 
