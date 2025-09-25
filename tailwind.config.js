582c76116ecd90d9f81fb415294de74f92ea608e
abrhot <abrihott@gmail.com>
2025-09-25 23:48:45 +0300

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,7 @@ const config = {
     "./src/app/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-  	container: {
-  		center: true,
-  		padding: '2rem',
-  		screens: {
-  			'2xl': '1400px'
-  		}
+ 
   	},
   	extend: {
   		fontFamily: {
