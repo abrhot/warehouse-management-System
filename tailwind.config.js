7b9d92de566833818a687bc6f47a8c8c23aa695f
abrhot <abrihott@gmail.com>
2025-09-25 23:52:36 +0300

    fix pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,4 @@
-import type { Config } from "tailwindcss"
 
-const config = {
-  darkMode: ["class"],
-  content: [
-    "./src/app/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-  ],
   theme: {
   	container: {
   		center: true,
