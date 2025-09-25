602549ad2bf86be67427923fc47b0c9cbc587ee2
abrhot <abrihott@gmail.com>
2025-09-25 23:50:11 +0300

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,11 @@
+import type { Config } from "tailwindcss"
 
+const config = {
+  darkMode: ["class"],
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
   	container: {
   		center: true,
